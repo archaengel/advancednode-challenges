ab97d9df93baaeccb9ea6865edb776c045359c2c
Glitch (archaengel-advancednode-challenges) <none>
2019-03-15 19:14:38 +0000

    👣🏮 Updated with Glitch - Add local authentication strategy using passport

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const session     = require('express-session');
-const passport    = require('passport');
-const ObjectID    = require('mongodb').ObjectID;
-const mongo       = require('mongodb').MongoClient;
+const express        = require('express');
+const bodyParser     = require('body-parser');
+const fccTesting     = require('./freeCodeCamp/fcctesting.js');
+const session        = require('express-session');
+const passport       = require('passport');
+const LocalStrategy  = require('passport-local');
+const ObjectID       = require('mongodb').ObjectID;
+const mongo          = require('mongodb').MongoClient;
 
 const app = express();
 
@@ -56,6 +57,18 @@ mongo.connect(process.env.DATABASE, {
         done(null, foundUser);
       });
     });
+    
+    passport.use(new LocalStrategy(
+      (username) => (password) => (done) => {
+        db.collection('users').findOne({username: username}, (err, user) => {
+          console.log(`User ${username} attempted to login`);
+          if (err)  { return done(err); }
+          if (!user) { return done(null, false); }
+          if (password !== user.password) {return done(null, false); }
+          return done(null, user);
+        })
+      }
+    ));
 
     app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + process.env.PORT);
