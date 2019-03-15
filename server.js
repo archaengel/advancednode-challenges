ca8f769aa8d9632b4bb1345065cfc8ac7c48504f
Glitch (archaengel-advancednode-challenges) <none>
2019-03-15 04:32:55 +0000

    😚👨‍❤️‍💋‍👨 Updated with Glitch - Set up Passport

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const session     = require('express-session');
+const passport    = require('passport');
 
 const app = express();
 
@@ -10,10 +12,22 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUnitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'pug');
 
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug', {
+      title: 'Hello',
+      message: 'Please login',
+    });
   });
 
 app.listen(process.env.PORT || 3000, () => {
